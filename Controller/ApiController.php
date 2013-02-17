0d0acd44dffc02742d8e3d8283abbf731b4e796c
corrupt <vetal.lg@tut.by>
2013-02-17 16:03:27 +0300

    Add api controller with common methods

---
+++ Controller/ApiController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Corrupt\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+abstract class ApiController extends Controller
+{
+    /**
+     * Convert form errors into array
+     *
+     * @param \Symfony\Component\Form\FormInterface $form
+     * @return array
+     */
+    protected function getErrorMessages(FormInterface $form)
+    {
+        $errors = array();
+
+        foreach ($form->getErrors() as $key => $error) {
+            $template = $error->getMessageTemplate();
+            $parameters = $error->getMessageParameters();
+
+            foreach ($parameters as $var => $value) {
+                $template = str_replace($var, $value, $template);
+            }
+
+            $errors[$key] = $template;
+        }
+
+        if (count($form->all()) > 0) {
+            foreach ($form->all() as $child) {
+                if (!$child->isValid()) {
+                    $errors[$child->getName()] = $this->getErrorMessages($child);
+                }
+            }
+        }
+
+        return $errors;
+    }
+
+    /**
+     * Create access denied exception
+     *
+     * @param string $message
+     * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    protected function createAccessDenied($message = '')
+    {
+        throw new HttpException(403, $message);
+    }
+}
